4e181ba09a355d251e67c00b9b73f38e16fbab3a
jinxiaojian <jinxiaojian@youxin.com>
2020-02-20 13:25:05 +0800

    fix

---
+++ dist/data2020.js
@@ -0,0 +1,39 @@
+/*
+ * @Description:
+ * @Author: jinxiaojian
+ * @Email: jinxiaojian@youxin.com
+ * @Date: 2019-07-05 10:42:44
+ * @LastEditTime: 2019-12-08 23:56:39
+ * @LastEditors: 靳肖健
+ */
+var dataArr2020 = [
+  // { info:'', title: ``,text:``, img: ``,link:[] },
+  {
+    info: "",
+    title: `观察者_发布订阅`,
+    text: ``,
+    img: `20/01/观察者_发布订阅.png`,
+    link: [],
+  },
+  {
+    info: "",
+    title: `浏览器渲染`,
+    text: ``,
+    img: `20/01/浏览器渲染.png`,
+    link: [],
+  },
+  {
+    info: "",
+    title: `ReduxVuex`,
+    text: ``,
+    img: `20/01/ReduxVuex.png`,
+    link: [],
+  },
+  {
+    info: "",
+    title: `页面兼容`,
+    text: ``,
+    img: `20/02/页面兼容 .png`,
+    link: [],
+  },
+];
